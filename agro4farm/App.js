6f90f81c3a9be16201ab9f7a09871b39641836bd
Łukasz Bociański <72608899+Ihavegot@users.noreply.github.com>
2022-03-31 15:23:07 +0200

    Functionality: Weather for choosen location

--- agro4farm/App.js
+++ agro4farm/App.js
@@ -1,40 +1,86 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState, useCallback } from 'react';
+import { StyleSheet, Text, View, TextInput, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 const Drawer = createDrawerNavigator();
-const Stack = createNativeStackNavigator();
 
-function HomeScreen({ navigation }) {
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title='Go to details'
-        onPress={() => {
-          navigation.navigate('Details')
-        }}
-      ></Button>
     </View>
   );
 }
 
-function DetailsScreen() {
+function WeatherScreen() {
+
+  const [weather, setWeather] = useState()
+  const [input, setInput] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const api = {
+    link: "https://api.openweathermap.org/data/2.5/",
+    key: "5eadd4755631169fa5a9ca425ce2f238"
+  }
+
+  const fetchWeather = useCallback(() => {
+    setIsLoading(true)
+    setInput('')
+    axios({
+      method: 'GET',
+      url: `${api.link}weather?q=${input}&units=metric&appid=${api.key}`
+    }).then(
+      res => {
+        // console.log(res.data)
+        setWeather(res.data)
+      }
+    ).catch(
+      e => console.dir(e)
+    ).finally(
+      () => setIsLoading(false)
+    )
+
+  }, [api.key, input])
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+      <TextInput
+        style={styles.cityInput}
+        placeholder='Choose city'
+        onChangeText={text => setInput(text)}
+        value={input}
+        onSubmitEditing={fetchWeather}
+      />
+      {isLoading && (
+        <ActivityIndicator
+          color="orange"
+          size="large"
+        />
+      )}
+      {weather && (
+        <View>
+          <Text style={styles.weatherDataLocation}>
+            {`${weather?.name} ${weather?.sys?.country}`}
+          </Text>
+          <Text style={styles.weatherDataTemperature}>
+            {`${Math.round(weather?.main?.temp)} Celsius`}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='Home'>
         <Drawer.Screen name='Home' component={HomeScreen} />
-        <Drawer.Screen name='Details' component={DetailsScreen} />
+        <Drawer.Screen name='Weather' component={WeatherScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -47,4 +93,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cityInput: {
+    padding: 10,
+    width: '50%',
+    backgroundColor: 'gray',
+    borderRadius: 50
+  },
+  weatherDataLocation: {
+    marginTop: 5,
+    textAlign: 'center',
+    color: 'orange',
+    fontSize: 30
+  },
+  weatherDataTemperature: {
+    textAlign: 'center',
+    color: 'orange',
+    fontSize: 20
+  }
 });
