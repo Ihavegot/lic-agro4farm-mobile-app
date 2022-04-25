7a852b694255aba4dec5100f2319788a4018f209
Łukasz Bociański <72608899+Ihavegot@users.noreply.github.com>
2022-04-25 12:32:15 +0200

    Added themes

--- agro4farm/App.js
+++ agro4farm/App.js
@@ -4,11 +4,12 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ApplicationNav } from './navigation/ApplicationNav'
+import { default as theme } from './A4Ftheme.json'
 
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
       <IconRegistry icons={EvaIconsPack} />
       <SafeAreaProvider>
         <ApplicationNav />
