ac01d099720414e453f22475176fbfb52d8ec36f
Łukasz Bociański <72608899+Ihavegot@users.noreply.github.com>
2022-05-06 12:03:12 +0200

    AboutScreen

--- agro4farm/screens/AboutScreen.js
+++ agro4farm/screens/AboutScreen.js
@@ -1,20 +1,28 @@
-import { Layout, Text } from '@ui-kitten/components'
+import { Layout, Text, useTheme } from '@ui-kitten/components'
 import React from 'react'
 import { StyleSheet } from 'react-native'
 
 export const AboutScreen = () => {
+    const theme = useTheme()
     const styles = StyleSheet.create({
         layout: {
             flex: 1,
-            paddingHorizontal: 15,
-            paddingVertical: 25,
-            justifyContent: 'center',
-            alignItems: 'center',
-        }
+            paddingHorizontal: 20,
+            paddingVertical: 35,
+        },
+        text:{
+            color: theme['color-primary-500'],
+        },
     })
     return (
         <Layout style={styles.layout}>
-            <Text category='h4'>Aplikacjia Agro4Farm to aplikacjia stworzona, aby ułatwić prace rolników.</Text>
+            <Text style={styles.text} category='h4'>Agro4Farm to aplikacjia stworzona, aby ułatwić prace rolników. W skłąd jej funkcjonalności wchodzą między innymi: </Text>
+            <Text></Text>
+            <Text style={styles.text} category='h5'>➊ Aktualna prognoza pogody</Text>
+            <Text style={styles.text} category='h5'>➋ Kalkulator wysiewu zbóż</Text>
+            <Text style={styles.text} category='h5'>➌ Notatnik</Text>
+            <Text style={styles.text} category='h5'>➍ Wyszukiwarka środków ochrony roślin</Text>
+            <Text style={styles.text} category='h5'>➎ Określanie najkorzystniejszych terminów do pracy/nawożenia</Text>
         </Layout>
     )
 }
